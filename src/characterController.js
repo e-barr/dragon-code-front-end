ab5a5f9905d40c88903e03b31998f7e717047e17
farverio <github email address>
2018-09-12 19:38:14 -0500

    updated image assets

--- src/characterController.js
+++ src/characterController.js
@@ -1,7 +1,9 @@
+const stepSize = 50//window.innerHeight / grid.length;
+
 class Character {
   constructor(){
     this.element = document.createElement('img');
-    this.speed = 15;
+    this.speed = 10;
     this.assets= 'assets/character',
     this.movement = null
     this.element.src = `${this.assets}/static.gif`
@@ -11,6 +13,46 @@ class Character {
     this.element.style.width = "50px"
     Character.all().push(this)
     document.body.appendChild(this.element)
+  }
+
+  stepEast(){
+    return new Promise( resolve => {
+      this.walkEast();
+      setTimeout( () => {
+        this.stop();
+        resolve()
+      }, stepSize / this.speed * 100)
+    })
+  }
+  
+  stepWest(){
+    return new Promise( resolve => {
+      this.walkWest();
+      setTimeout( () => {
+        this.stop();
+        resolve()
+      }, stepSize / this.speed * 100)
+    })
+  }
+  
+  stepNorth(){
+    return new Promise( resolve => {
+      this.walkNorth();
+      setTimeout( () => {
+        this.stop();
+        resolve()
+      }, stepSize / this.speed * 100)
+    })
+  }
+  
+  stepSouth(){
+    return new Promise( resolve => {
+      this.walkSouth();
+      setTimeout( () => {
+        this.stop();
+        resolve()
+      }, stepSize / this.speed * 100)
+    })
   }
 
   walkEast(){
@@ -60,3 +102,26 @@ class Character {
 }
 
 Character.prototype.all = new Array
+
+document.addEventListener("DOMContentLoaded", () => {
+  char = new Character
+  
+  function moveCharacter(e) {
+
+    if (e.keyCode == 39) {
+      e.preventDefault()
+      char.stepEast()
+    } else if (e.keyCode == 40) {
+      e.preventDefault()
+      char.stepSouth()
+    } else if (e.keyCode == 37) {
+      e.preventDefault()
+      char.stepWest()
+    } else if (e.keyCode == 38) {
+      e.preventDefault()
+      char.stepNorth()
+    }
+  }
+  
+  window.addEventListener('keydown', e => moveCharacter(e))
+})
